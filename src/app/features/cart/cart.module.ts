8ea32f75d06b6b990cd40cf5fe1f8b7a9dc34711
br1ttan <brittanvladislav@gmail.com>
2023-02-07 19:36:47 +0200

    (refactor) deleted unuse empty cart and added loader to cart dialog

--- src/app/features/cart/cart.module.ts
+++ src/app/features/cart/cart.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EmptyCartComponent } from './components';
 
 @NgModule({
-  declarations: [
-    EmptyCartComponent
-  ],
   imports: [
     CommonModule,
   ],
-  exports: [
-    EmptyCartComponent
-  ]
 })
 export class CartModule { }
--- src/app/features/cart/index.ts
+++ src/app/features/cart/index.ts
@@ -1,3 +1,2 @@
 export * from './cart.module';
 export * from './services';
-export * from './components';
